3db80afff6fc7f6c34bad62da92b96fd729607fe
nivagator <gavingreer@mac.com>
2020-02-23 13:55:32 -0600

    split changeUnits to its own function

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ Simple browser based weather app. Initially created by following [this Dev Ed gu
 Uses the [DarkSky API](https://darksky.net/dev) for weather data and [Skycons](https://darkskyapp.github.io/skycons/) for canvas icons. For localhost development, the [CORS-anywhere Heroku app](https://cors-anywhere.herokuapp.com/) is used. 
 
 ### Project todo
-  - update timezone to display city/state
+  - ~~update timezone to display city/state~~
   - review darksky data and integrate more features 
     - daily/hourly forecast
-  - change degree number formatting to no decimals for F and one for C
+    - ~~add apparent temp~~
+  - ~~change degree number formatting to no decimals for F and one for C~~
   - remove dependence on CORS-Anywhere proxy
   - clean javascript code, separate out specific functions from fetch(api)
     - review the [Mozilla developer site for geolocation](https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API) for JS function ideas.
+    - ~~split out function for changing units~~
   - Browser Location:
     - add default location 
     - add button to get user's location (maybe) - or simply try to get location
--- app.js
+++ app.js
@@ -13,25 +13,25 @@ window.addEventListener('load', ()=> {
 
   //if location exists in the browser
   if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position =>{
-      console.log(position); // show in browser console
+    navigator.geolocation.getCurrentPosition(position => {
+      // console.log(position); // show in browser console
       long = position.coords.longitude;
       lat = position.coords.latitude;
       
       // add condition if localhost, use proxy
       const proxy = 'https://cors-anywhere.herokuapp.com/';
       const api = `${proxy}https://api.darksky.net/forecast/6641aefb61ccf2f202746e2db88500d3/${lat},${long}?exclude=[hourly,minutely]`;
-      console.log(api)
+      // console.log(api)
       const revapi =`https://us1.locationiq.com/v1/reverse.php?key=d3afc63e6da6d4&lat=${lat}&lon=${long}&format=json`
-      console.log(revapi)
+      // console.log(revapi)
 
       //location data
       fetch(revapi)
         .then(response => {
           return response.json();
         })
-        .then(revdata =>{
-          console.log(revdata);
+        .then(revdata => {
+          // console.log(revdata);
           const { city, state } = revdata.address;
           // set dom elements from rev api
           locTimezone.textContent = `${city}, ${state}`
@@ -39,12 +39,12 @@ window.addEventListener('load', ()=> {
       
       // weather data   
       fetch(api)
-        .then(response =>{
+        .then(response => {
           //convert to json
           return response.json();
         })
         .then(data => {
-          console.log(data);
+          // console.log(data);
           const { temperature, summary, icon, time, apparentTemperature } = data.currently;
 
           //Set DOM elements from the api
@@ -53,36 +53,42 @@ window.addEventListener('load', ()=> {
           feelsDegrees.textContent = Math.round(apparentTemperature)
           //locTimezone.textContent = data.timezone
 
-          // formula for celcius 
-          let celcius = (temperature - 32) * (5 / 9);
-          let feelsCelcius = (apparentTemperature -32) * (5 / 9);
-
           //set icon
           setIcons(icon, document.querySelector('.icon'));
 
           // change temp to C/F
-          degreeSection.addEventListener('click', () =>{
-            if(degreeSpan.textContent === "F") {
-              degreeSpan.textContent = "C";
-              degreeSection.textContent = celcius.toFixed(1); //C to one decimal
-              feelSpan.textContent = "C";
-              feelsDegrees.textContent = feelsCelcius.toFixed(1); //C to one decimal
-            }else{
-              degreeSpan.textContent = "F";
-              degreeSection.textContent = Math.round(temperature); //F to no decimals
-              feelSpan.textContent = "F";
-              feelsDegrees.textContent = Math.round(apparentTemperature); //F to no decimals
-            }
+          degreeSection.addEventListener('click', function(){
+            changeUnits(temperature,apparentTemperature);
+          });
           })
         });
-    });
+    };
   //}else{ // or default it to a location
   //  h1.textContent = 'app requires location services.'
-  }
+  
+  // function set icons
   function setIcons(icon, iconID){
     const skycons = new Skycons({color: "white"});
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
     skycons.play();
     return skycons.set(iconID, Skycons[currentIcon])
+  }
+
+  // function to switch between F and C
+  function changeUnits(temp,feelstemp){
+    if(degreeSpan.textContent === "F") {
+      // formula for celcius 
+      let celcius = (temp - 32) * (5 / 9);
+      let feelsCelcius = (feelstemp -32) * (5 / 9);
+      degreeSpan.textContent = "C";
+      degreeSection.textContent = celcius.toFixed(1); //C to one decimal
+      feelSpan.textContent = "C";
+      feelsDegrees.textContent = feelsCelcius.toFixed(1); //C to one decimal
+    }else{
+      degreeSpan.textContent = "F";
+      degreeSection.textContent = Math.round(temp); //F to no decimals
+      feelSpan.textContent = "F";
+      feelsDegrees.textContent = Math.round(feelstemp); //F to no decimals
+    }
   }
 });
